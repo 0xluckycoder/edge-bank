55ccbbd1962d8e0ee268133720b64b6996fd92ad
0xlakshan <lakshanperera.dev@gmail.com>
2022-06-21 00:31:02 +0530

    completed guidance

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import './style.css';
-import BannerImage from './images/banner-img.svg'
+import BannerImage from './images/banner-img.svg';
+import Commercial from './images/commercial.png';
+import Loan from './images/loan.png';
+import Personal from './images/personal.png';
+import CreditCard from './images/credit-card.png';
+import ListDisc from './images/list-disc.png';
 
-// banner image
-const bannerImg = document.querySelector('#banner-img');
-bannerImg.src = BannerImage;
+// inserting images
+document.querySelector('#banner-img').src = BannerImage;;
+document.querySelector('#commercial').src = Commercial;
+document.querySelector('#personal').src = Personal;
+document.querySelector('#loan').src = Loan;
+document.querySelector('#credit-card').src = CreditCard;
+
+let listNodeList = document.querySelectorAll('#list-img');
+for (let nodeListItem of listNodeList) {
+    nodeListItem.src = ListDisc;
+}
+
+
+
+
+// hamburger trigger
+const hamburgerButton = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+let isMenuOpened = false;
+
+hamburgerButton.addEventListener('click', (e) => {
+    if (isMenuOpened) {
+        // closing
+        mobileMenu.classList.add('hidden');
+        isMenuOpened = false;
+    } else {
+        // opening
+        mobileMenu.classList.remove('hidden');
+        mobileMenu.classList.add('open');
+        isMenuOpened = true;
+    }
+})
